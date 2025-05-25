2154d31b2e88995a77a9a538e01aecef5c1cfcac
Nordik <igor.golyshev.01@bk.ru>
2025-05-25 15:01:37 +0300

    feat : TestScripts

---
+++ Assets/_Project/Scripts/RotationComponent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Project.Scripts
+{
+    public class RotationComponent : MonoBehaviour
+    {
+        [SerializeField] private float _rotationSpeed;
+        
+        private void Update()
+        {
+            transform.Rotate(transform.forward, _rotationSpeed * Time.deltaTime);
+        }
+    }
+}
