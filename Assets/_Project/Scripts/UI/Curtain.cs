d24e2d623e2c5279d66a522c55af62250176149d
Nordik <igor.golyshev.01@bk.ru>
2025-05-25 15:29:32 +0300

    feat : AddCurtainLogic

---
+++ Assets/_Project/Scripts/UI/Curtain.cs
@@ -0,0 +1,25 @@
+using DG.Tweening;
+using UnityEngine;
+
+namespace _Project.Scripts.UI
+{
+    public class Curtain : MonoBehaviour
+    {
+        [SerializeField] private CanvasGroup _canvasGroup;
+        [SerializeField] private float _animationDuration;
+        
+        public void Show()
+        {
+            gameObject.SetActive(true);
+            _canvasGroup.alpha = 1;
+        }
+
+        public void Hide()
+        {
+            _canvasGroup
+                .DOFade(0, _animationDuration)
+                .From(1)
+                .OnComplete(() => gameObject.SetActive(false));
+        }
+    }
+}
